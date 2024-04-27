058414a7207508c8459a52e17e3fd96bbb853de1
ZeusORIG <162359084+ZeusORIG@users.noreply.github.com>
2024-04-27 23:16:10 +0300

    v ap0.0.0.2(5)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,4 +7,16 @@ public class GameManager : MonoBehaviour
     public void Play(){
         CutsceneManager.Instance.StartCutscene("PlayCutscene"); // Запуск катсцены по ключу
     }
+
+    public void Settings(){
+        CutsceneManager.Instance.StartCutscene("SettingsCutscene"); // Запуск катсцены по ключу
+    }
+
+    public void ExitPlay(){
+        CutsceneManager.Instance.StartCutscene("ExitPlayCutscene"); // Запуск катсцены по ключу
+    }
+
+    public void ExitSettings(){
+        CutsceneManager.Instance.StartCutscene("ExitSettingsCutscene"); // Запуск катсцены по ключу
+    }
 }
